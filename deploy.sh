fb8956a10c7089aded53bd87b56a3a252493a707
Somik Raha <somikr@gmail.com>
2015-11-05 22:05:39 -0800

    Copied fonts over

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ echo "Copying dist files over"
 cp dist/bundle.css /var/www/html/brainstormer/
 cp dist/bundle.js /var/www/html/brainstormer/
 cp dist/index.html /var/www/html/brainstormer/
+cp dist/fonts /var/www/html/brainstormer/ -R
 echo "Deployment complete."
