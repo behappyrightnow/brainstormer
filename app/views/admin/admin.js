a149c3b25bc1ad9201fcc2a7aaac772ee13b873c
Somik Raha <somikr@gmail.com>
2015-11-08 15:18:31 -0800

    Missing admin code

---
+++ app/views/admin/admin.js
@@ -0,0 +1,69 @@
+///<reference path="../../lib/vendorTypeDefinitions/angular.d.ts"/>
+///<reference path="../../lib/vendorTypeDefinitions/firebase.d.ts"/>
+///<reference path="../../app.ts"/>
+'use strict';
+var astro;
+angular.module('brainstormer.admin', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/admin', {
+            templateUrl: 'views/admin/admin.html',
+            controller: 'AdminCtrl'
+        });
+    }])
+    .controller('AdminCtrl', ['$scope', '$location', '$route', 'firebase', function ($scope, $location, $rootScope, firebase) {
+        var adminDataRef = firebase.admin;
+        var storyDataRef = firebase.stories;
+        $scope.stories = new Array();
+        storyDataRef.on('child_added', function (snapshot) {
+            var newStory = snapshot.val();
+            $scope.stories.push(newStory);
+            updateScope($scope);
+        });
+        storyDataRef.on('child_changed', function (snapshot) {
+            var newStory = snapshot.val();
+            for (var i = 0; i < $scope.stories.length; i++) {
+                var story = $scope.stories[i];
+                if (story.storyID === newStory.storyID) {
+                    story.summary = newStory.summary;
+                    story.story = newStory.story;
+                    story.powerful = newStory.powerful;
+                    story.interesting = newStory.interesting;
+                    story.name = newStory.name;
+                    story.imageURL = newStory.imageURL;
+                    story.deleteMode = false;
+                }
+            }
+            console.log("Updated " + newStory.name + ", summary: " + newStory.summary + ", story: " + newStory.story);
+            updateScope($scope);
+        });
+        $scope.delete = function (card) {
+            card.deleteMode = true;
+        };
+        $scope.actuallyDelete = function (card) {
+            var storyToDelete = null;
+            var storyIndex = -1;
+            for (var i = 0; i < $scope.stories.length; i++) {
+                var story = $scope.stories[i];
+                if (story.storyID === card.storyID) {
+                    storyToDelete = story;
+                    storyIndex = i;
+                    break;
+                }
+            }
+            if (storyToDelete !== null) {
+                var storyRef = new Firebase(firebase.storyURL + storyToDelete.storyID);
+                storyRef.remove(function (error) {
+                    if (error) {
+                        console.log('Remove failed');
+                    }
+                    else {
+                        console.log('Remove succeeded');
+                        card.deleteMode = false;
+                        delete $scope.stories[storyIndex];
+                        updateScope($scope);
+                    }
+                });
+            }
+        };
+    }]);
+//# sourceMappingURL=admin.js.map
